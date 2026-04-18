dfdf8b0edd3f2feefd3dc0093bcca130a157b809
nick16384 <theophil@pudelkern.com>
2026-04-18 15:29:14 +0200

    Deutsch

---
+++ c/deutsch/Deutsch.h
@@ -0,0 +1,14 @@
+#ifndef _DEUTSCH_H
+#define _DEUTSCH_H
+#endif
+
+// #define beinhalte include
+#define Hauptprozedur main
+#define Fehlender_Parameter void
+#define zurück return
+#define Schreibe_Auf_Konsole_Formattiert printf
+#define Zeilenumbruch "\n"
+
+#include <stdio.h>
+
+typedef int Ganze_Zahl;
---
+++ c/deutsch/Haupt.c
@@ -0,0 +1,7 @@
+#include "Deutsch.h"
+
+Ganze_Zahl Hauptprozedur(Fehlender_Parameter)
+{
+    Schreibe_Auf_Konsole_Formattiert("Hallo, Welt!%s", Zeilenumbruch);
+    zurück 0;
+}
